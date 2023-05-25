3b493b6cba0ef823840ff301c7796a65da2e6a9a
a <ss@qq.com>
2023-05-25 20:30:30 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
   function onClick() {}
   return (
-    <button id="btn" onClick={onClick} key="123">
-      Click Me
-    </button>
+    <Foo>
+      <button id="btn" onClick={onClick} key="123">
+        Click Me
+      </button>
+    </Foo>
   );
+}
+
+function Foo({ children }) {
+  return <div>{children}</div>;
 }
 export default App;
